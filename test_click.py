eebceee45e5bf6ed5feda5842ee3a547a1030255
ildarneo <ildar_talipov@mail.ru>
2025-12-12 15:30:22 +0300

    1

--- test_click.py
+++ test_click.py
@@ -5,6 +5,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture()
@@ -34,18 +37,33 @@ class TestClick:
         driver = set_up_browser
         driver.get("https://www.aviasales.ru/search/KZN1501LED18011")
         #print("Заголовок страницы: ", driver.title)
-        driver.find_element(By.XPATH, '//button').click()
+        driver.find_element(By.XPATH, "//button[@data-test-id='profile-button']").click()
         pass
         time.sleep(5)
+        driver.quit()
         pass
 
-    def doubleclick_clickavia(self, set_up_browser):
+    def test_clickavia1(self, set_up_browser):
+        #поиск элемента, ожидание и клик
         driver = set_up_browser
-        driver.get("https://www.aviasales.ru/search/KZN1501LED18011")
-        print("Заголовок страницы: ", driver.title)
-        action_chains = webdriver.ActionChains(driver)
-        action_chains.double_click(driver.find_element(By.XPATH, '//button'))
+        driver.get("https://www.aviasales.ru/")
+        #print("Заголовок страницы: ", driver.title)
+        profile_button = WebDriverWait(driver, 4).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@data-test-id='profile-button']"))
+        )
+        profile_button.click()
 
-        pass
         time.sleep(5)
+        driver.quit()
         pass
+
+    def testdoubleclick_google(self, set_up_browser):
+        #двойной клик
+        driver = set_up_browser
+        driver.get("https://www.google.com/")
+        print("Заголовок страницы: ", driver.title)
+        assert "Google" == driver.title
+        action_chains = webdriver.ActionChains(driver)
+        action_chains.double_click(driver.find_element(By.XPATH, "//a[contains(@aria-label, 'Войти')]")).perform()
+        time.sleep(2)
+        driver.quit()
