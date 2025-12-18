614fc3a36fccff36c7491953604f02ba116a9eac
ildarneo <ildar_talipov@mail.ru>
2025-12-18 17:30:04 +0300

    1

---
+++ module5/conftest.py
@@ -0,0 +1,25 @@
+pytest_plugins = [
+    "src.browser"
+]
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+
+path_to_driver = 'C:/Chromedriver/chromedriver.exe' # это пример пути как нужно указать а так driver нужно скачать от сюда https://googlechromelabs.github.io/chrome-for-testing/ и тут указать путь до него
+
+
+# @pytest.fixture()
+# def set_up_browser():
+#     options = Options()
+#     options.page_load_strategy = 'eager'
+#     # Создаем драйвер локально с помощью webdriver_manager
+#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#     yield driver
+#     driver.quit()
+
+
