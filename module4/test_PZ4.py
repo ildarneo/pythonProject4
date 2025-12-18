c316e8c5176af0f01683a71430c7ef5416942b8e
ildarneo <ildar_talipov@mail.ru>
2025-12-18 12:56:49 +0300

    1

--- module4/test_PZ4.py
+++ module4/test_PZ4.py
@@ -8,6 +8,11 @@ import time
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture()
 def set_up_browser():
@@ -39,8 +44,10 @@ class TestPraktik4:
         driver.find_element(By.XPATH, "//span[text()='Author']").click()
         search_input = driver.find_element(By.CSS_SELECTOR, "input[aria-label='Filter authors']")
         search_input.send_keys("bpasero")
+        search_input.click()
         time.sleep(3)
-        driver.find_element(By.CLASS_NAME, "prc-ActionList-ItemLabel-TmBhn").click()
+        #driver.find_element(By.CLASS_NAME, "prc-ActionList-ItemLabel-TmBhn").click()
+
         time.sleep(3)
         driver.quit()
 
@@ -69,57 +76,53 @@ class TestPraktik4:
         driver = set_up_browser
         driver.get("https://skillbox.ru/code/")
         driver.maximize_window()
-        time.sleep(5)
+        time.sleep(2)
 
         driver.find_element(By.XPATH, "//button[contains(text(), 'Фильтры')]").click()
-        driver.find_element(By.XPATH, "//span[contains(@class, 'ui-tab__text') and contains(text(), 'Профессия')]") \
-            .click()
+        driver.find_element(By.XPATH,
+                            "//span[contains(@class, 'ui-tab__text') and contains(text(), 'Профессия')]").click()
 
-        driver.find_element(By.XPATH,
-                            "//span[contains(@class, 'ui-tab__text f f--m') and contains(text(), 'От 6 до 12 мес.')]") \
-            .click()
+        time.sleep(2)
+        el = driver.find_element(By.XPATH,
+                                 "//span[contains(@class, 'ui-tab__text') and contains(text(), 'От 6 до 12 мес.')]")
 
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", el)
+        el.click()
         driver.find_element(By.XPATH,
-                            "//li[contains(text(), 'От 6 до 12 мес.')]") \
-            .click()
-        driver.find_element(By.XPATH,
-                            "//span[contains(text(), 'Тематика')]") \
-            .click()
+                            "//span[contains(@class, 'ui-tab__text') and contains(text(), 'Android')]").click()
         time.sleep(3)
-        driver.find_element(By.XPATH, "//li[contains(text(), 'Pentest')]") \
-            .click()
         driver.find_element(By.XPATH, "//button[contains(text(), 'Применить')]") \
             .click()
 
         time.sleep(3)
         driver.quit()
 
-    def test_case4_old(self, set_up_browser):
-        driver = set_up_browser
-        driver.get("https://skillbox.ru/code/")
-        driver.maximize_window()
-        time.sleep(5)
-
-        driver.find_element(By.XPATH,
-                            "//span[contains(text(), 'Профессия')]").click()
-
-        driver.find_element(By.XPATH,
-                            "//span[contains(@class, 'ui-round-select__field-title') and contains(text(), 'Длительность')]") \
-            .click()
-        driver.find_element(By.XPATH,
-                            "//li[contains(text(), 'От 6 до 12 мес.')]") \
-            .click()
-        driver.find_element(By.XPATH,
-                            "//span[contains(text(), 'Тематика')]") \
-            .click()
-        time.sleep(3)
-        driver.find_element(By.XPATH, "//li[contains(text(), 'Pentest')]") \
-            .click()
-        driver.find_element(By.XPATH, "//button[contains(text(), 'Применить')]") \
-            .click()
-
-        time.sleep(3)
-        driver.quit()
+    # def test_case4_old(self, set_up_browser):
+    #     driver = set_up_browser
+    #     driver.get("https://skillbox.ru/code/")
+    #     driver.maximize_window()
+    #     time.sleep(5)
+    #
+    #     driver.find_element(By.XPATH,
+    #                         "//span[contains(text(), 'Профессия')]").click()
+    #
+    #     driver.find_element(By.XPATH,
+    #                         "//span[contains(@class, 'ui-round-select__field-title') and contains(text(), 'Длительность')]") \
+    #         .click()
+    #     driver.find_element(By.XPATH,
+    #                         "//li[contains(text(), 'От 6 до 12 мес.')]") \
+    #         .click()
+    #     driver.find_element(By.XPATH,
+    #                         "//span[contains(text(), 'Тематика')]") \
+    #         .click()
+    #     time.sleep(3)
+    #     driver.find_element(By.XPATH, "//li[contains(text(), 'Pentest')]") \
+    #         .click()
+    #     driver.find_element(By.XPATH, "//button[contains(text(), 'Применить')]") \
+    #         .click()
+    #
+    #     time.sleep(3)
+    #     driver.quit()
 
     def test_case5(self, set_up_browser):
         driver = set_up_browser
